abe275ae76935379bbe8f336a54ca15ca46d215d
haze <haze@otherside.sh>
2026-03-23 09:30:24 -0700

    abstract this stupid shit

--- lua/chomp/init.lua
+++ lua/chomp/init.lua
@@ -7,25 +7,10 @@ local M = {}
 M.setup = function(opts)
   config.set(opts or {})
   local _config = config.current
-  -- TODO: currently just load entire db into state, should make it queryable or less wasteful..
   local _state = state.load()
 
-  local state_feeds = {}
-  for f in _state.feeds do
-    state_feeds[f.url] = f
-  end
-
-  local feeds_tmp = {}
-  for f in _config.feeds do
-    local normalized = util.url.normalize(f)
-    if state_feeds[normalized] then
-      table.insert(feeds_tmp, state_feeds[normalized])
-    else
-      table.insert(feeds_tmp, _state.new_feed(normalized))
-    end
-  end
-  _state.feeds = feeds_tmp
-
+  -- config feeds are definitive
+  _state.sync_feeds(_config.feeds)
   _state.dump()
 
   require 'chomp.api.commands'
--- lua/chomp/state.lua
+++ lua/chomp/state.lua
@@ -1,4 +1,5 @@
 local db = require 'chomp.db'
+local util = require 'chomp.util'
 
 local M = {}
 
@@ -19,6 +20,32 @@ M.dump = function() db.save(state) end
 M.mark_read = function(feed, id)
   if state.feeds and state.feeds[feed] then table.insert(state.feeds[feed].read, id) end
 end
+
+local new_feed = function(url)
+  local data = util.http.get(url)
+  local _parser = util.parser.new(db.cache.save_feed(data.xml))
+
+  return {
+    title = _parser:title(),
+    last_updated = _parser:last_updated(),
+    etag = data.etag,
+    read = {},
+    posts = _parser:posts(),
+  }
+end
+
+M.sync_feeds = function(urls)
+  local feeds_tmp = {}
+  for u in urls do
+    local normalized = util.url.normalize(u)
+    feeds_tmp = state.feeds[normalized] or new_feed(normalized)
+  end
+  state.feeds = feeds_tmp
+end
+
+M.get_post = function(id) end
+
+M.is_updated = function(url) end
 
 M.current = state
 
