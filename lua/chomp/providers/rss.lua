8f0a2438c5747e49c944a82a49084ca869015d34
haze <haze@otherside.sh>
2026-05-13 14:13:30 -0700

    implement rss posts parsing

--- lua/chomp/providers/rss.lua
+++ lua/chomp/providers/rss.lua
@@ -5,8 +5,8 @@ Base.__index = Base
 
 ---@class Rss20
 ---@field _title string
----@field _updated string|nil
----@field _els table[]
+---@field _updated string?
+---@field _els table<XmlElement>
 ---@field _items_from integer
 ---@field _doc XmlDocument
 local Rss20 = setmetatable({}, Base)
@@ -36,20 +36,54 @@ Rss20.new = function(doc)
     i = i + 1
   end
 
-  local meta = xml.extract({ 'title', 'lastBuildDate' }, { table.unpack(channel_els, 1, i - 1) })
-  self._title = meta.title
-  self._updated = meta.lastBuildDate
+  local meta = xml.extract({
+    title = { dst = '_title', func = xml.get_element_text },
+    lastBuildDate = { dst = '_updated', func = xml.get_element_text },
+  }, { table.unpack(channel_els, 1, i - 1) })
+
+  self = vim.tbl_extend('force', self, meta)
   self._items_from = i
   self._els = channel_els
 
   return self
 end
 
-Rss20.get_title = function(self) end
+Rss20.get_title = function(self) return self._title end
 
-Rss20.get_updated = function(self) end
+Rss20.get_updated = function(self) return self._updated end
 
-Rss20.posts = function(self) end
+---@return fun(): Post?
+Rss20.posts = function(self)
+  local i = self._items_from - 1
+
+  local value_mappings = {
+    title = { dst = 'title', func = xml.get_element_text },
+    link = { dst = 'url', func = xml.get_element_text },
+    guid = { dst = 'guid', func = xml.get_element_text },
+    description = { dst = 'summary', func = xml.get_element_text },
+    pubDate = {
+      dst = 'published_at',
+      func = function(x)
+        -- convert date to ts
+        return xml.get_element_text(x)
+      end,
+    },
+  }
+
+  ---@return Post?
+  return function()
+    local ret = {}
+    i = i + 1
+    if not self._els[i] then return end
+    while self._els[i].name ~= 'item' do
+      i = i + 1
+    end
+
+    ret = xml.extract(value_mappings, self._els[i].el)
+
+    return ret
+  end
+end
 
 return {
   ['2.0'] = Rss20,
